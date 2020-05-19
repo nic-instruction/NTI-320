1b4936377aa4991e71434d3255be4240703fef57
nic-instruction <nicole.bade@seattlecolleges.edu>
2020-05-19 14:28:58 -0700

    Update apache.sh

--- terraform/apache.sh
+++ terraform/apache.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sed -i 's/^/#/g' /etc/httpd/conf.d/welcome.conf                                         # Comment out the default welcome page
-echo "<html><body><h1>Hi there NTI 320</h1></body></html>" > /var/www/html/index.html   # Create custom welcome page
-systemctl restart httpd                                                                 # Restart apache so our changes take effect
+sudo rm /var/www/html/index.html                                                            # delete the default apache page
+sudo echo "<html><body><h1>Hi there NTI 320</h1></body></html>" > /var/www/html/index.html  # Create custom welcome page
+sudo systemctl restart apache2                                                              # Restart apache so our changes take effect
