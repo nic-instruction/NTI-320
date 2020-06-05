64ad6bf2c2740823228e77e5f660ca8109ad832c
nic-instruction <nicole.bade@seattlecolleges.edu>
2020-06-04 17:44:42 -0700

    Update scp_to_nagios.sh

--- automation/scp_to_nagios.sh
+++ automation/scp_to_nagios.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 bash generate_config.sh $1 $2
-gcloud compute scp --zone us-east4-c $1.cfg nicolebade@nagios-a:/etc/nagios/servers
+gcloud compute scp --zone us-central1-a $1.cfg nicolebade@nagios:/etc/nagios/servers
 # Note: I had to add user nicolebade to group nagios using usermod -a -G nagios nicolebade
 # I also had to chmod 777 /etc/nagios/servers
-gcloud compute ssh --zone us-east4-c nicolebade@nagios-a --command='sudo /usr/sbin/nagios -v /etc/nagios/nagios.cfg'
+gcloud compute ssh --zone us-central1-a nicolebade@nagios --command='sudo /usr/sbin/nagios -v /etc/nagios/nagios.cfg'
