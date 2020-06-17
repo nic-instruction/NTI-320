69360f3ac8f4508f6db778a52ab7c7cb28ded169
nic-instruction <nicole.bade@seattlecolleges.edu>
2020-06-17 12:57:11 -0700

    Update install_repo_and_rpm.sh

--- automation/install_repo_and_rpm.sh
+++ automation/install_repo_and_rpm.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# This script adds our rep to all the servers except cacti and nagios and installs the nti-320-plugins rpm
+# which contains clients for: logsrv, nagios, and cacti (nrpe, snmp, and rsyslog)
 
-gcloud compute scp --zone us-central1-a add_repo.sh nicolebade@$server:/tmp/add_repo.sh
-gcloud compute ssh --zone us-central1-a nicolebade@$server --command='sudo chmod +x /tmp/add_repo.sh; sudo /tmp/add_repo.sh; sudo yum -y --enablerepo=nti-320 clean metadata; sudo yum -y install nti-320-plugins'
+user="nicolebade"      #put your gcloud username here
+
+for servername in $( gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v nagios | grep -v cacti );  do
+   gcloud compute scp --zone us-central1-a add_repo.sh $user@$server:/tmp/add_repo.sh
+   gcloud compute ssh --zone us-central1-a $user@$server --command='sudo chmod +x /tmp/add_repo.sh; sudo /tmp/add_repo.sh; sudo yum -y --enablerepo=nti-320 clean metadata; sudo yum -y install nti-320-plugins'
+done
