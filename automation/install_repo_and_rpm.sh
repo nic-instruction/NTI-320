0f74fbeb8d7d147a550f36f91c41d618f2728401
nic-instruction <nicole.bade@seattlecolleges.edu>
2020-06-17 12:57:35 -0700

    Update install_repo_and_rpm.sh

--- automation/install_repo_and_rpm.sh
+++ automation/install_repo_and_rpm.sh
@@ -4,7 +4,7 @@
 
 user="nicolebade"      #put your gcloud username here
 
-for servername in $( gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v nagios | grep -v cacti );  do
+for server in $( gcloud compute instances list | awk '{print $1}' | sed "1 d" | grep -v nagios | grep -v cacti );  do
    gcloud compute scp --zone us-central1-a add_repo.sh $user@$server:/tmp/add_repo.sh
    gcloud compute ssh --zone us-central1-a $user@$server --command='sudo chmod +x /tmp/add_repo.sh; sudo /tmp/add_repo.sh; sudo yum -y --enablerepo=nti-320 clean metadata; sudo yum -y install nti-320-plugins'
 done
